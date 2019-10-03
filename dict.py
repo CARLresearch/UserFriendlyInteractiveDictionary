d27ac71c846d319647ac0bd89c12f69ade62374e
sulaksh3008 <54094858+sulaksh3008@users.noreply.github.com>
2019-10-03 11:35:26 -0700

    dict created

---
+++ dict.py
@@ -0,0 +1,12 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data(2).json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
